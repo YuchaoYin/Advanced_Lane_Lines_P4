8fb43ec1ebad87064b4f7f6c4e989e9a9d8e6aab
York_Yin <yinyuchao1990@hotmail.com>
2017-12-26 09:06:33 +0800

    Add files via upload

---
+++ camera_calibration.py
@@ -0,0 +1,76 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import pickle
+
+def calibration(img, pickle_file, cali_path, verbose=False):
+
+    # Check if calibration already computed
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as load_file:
+            data = pickle.load(load_file)
+        objpoints = data['objpoints']
+        imgpoints = data['imgpoints']
+    else:
+        # Array to store object points and image points
+        objpoints = []
+        imgpoints = []
+
+        for images in os.listdir(cali_path):
+
+            # Read in a image
+            image = mpimg.imread(os.path.join(cali_path, images))
+
+            # Prepare object points like (0,0,0),(1,0,0)...
+            objp = np.zeros((6*9,3),np.float32)
+            objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+            # Convert to grayscale
+            gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+            # Find the chessboard corners
+            ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+
+            # If corners are found, add object points, image points
+            if ret == True:
+                imgpoints.append(corners)
+                objpoints.append(objp)
+
+                if verbose == True:
+                    # Draw and display
+                    cv2.drawChessboardCorners(image, (9, 6), corners, ret)
+                    cv2.imshow('Draw Corners', image)
+
+
+        with open(pickle_file, 'wb') as dump_file:
+            data = {'objpoints':objpoints, 'imgpoints':imgpoints}
+            pickle.dump(data, dump_file)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    return ret, mtx, dist, rvecs, tvecs
+
+def undistort(img, mtx, dist, verbose=False):
+
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+
+    if verbose == True:
+        plt.imshow(dst)
+        plt.title('Undistorted Image')
+        plt.show()
+
+    return dst
+
+if __name__ == '__main__':
+    cali_path = '/home/yuchao/CarND-Advanced-Lane-Lines/camera_cal'
+    pickle_file = '/home/yuchao/CarND-Advanced-Lane-Lines/pick'
+    img = mpimg.imread('/home/yuchao/CarND-Advanced-Lane-Lines/camera_cal/calibration2.jpg')
+    ret, mtx, dist, rvecs, tvecs = calibration(img, pickle_file, cali_path, verbose=False)
+    dst = undistort(img, mtx, dist, verbose=True)
+    #cv2.imwrite('/home/yuchao/CarND-Advanced-Lane-Lines/output_images/original_image.jpg', img)
+    #cv2.imwrite('/home/yuchao/CarND-Advanced-Lane-Lines/output_images/undistorted_image.jpg', dst)
+
+
---
+++ image_binarization.py
@@ -0,0 +1,96 @@
+import numpy as np
+import cv2
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import sys
+
+'''
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(binary_output, cmap='gray')
+ax2.set_title('Thresholded Gradient', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
+'''
+def abs_sobel_thresh(img, orient, sobel_kernel, thresh):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    if orient == 'x':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    elif orient == 'y':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    else:
+        print('wrong argument!')
+        sys.exit(1)
+    abs_sobel = np.absolute(sobel)
+    scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
+    grad_binary = np.zeros_like(scaled_sobel)
+    grad_binary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+
+    return grad_binary
+
+def mag_thresh(img, sobel_kernel, mag_thresh):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    scale_factor = np.max(gradmag) / 255
+    gradmag = (gradmag / scale_factor).astype(np.uint8)
+    mag_binary = np.zeros_like(gradmag)
+    mag_binary[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+
+    return mag_binary
+
+def dir_threshold(img, sobel_kernel, thresh):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    dir_binary = np.zeros_like(absgraddir)
+    dir_binary[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+
+    return dir_binary
+
+def color_threshold(img, thresh):
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    S = hls[:, :, 2]
+    S_binary = np.zeros_like(S)
+    S_binary[(S > thresh[0]) & (S_binary <= thresh[1])] = 1
+
+    return S_binary
+
+def morphology(img, ksize):
+    kernel = np.ones(ksize, np.uint8)
+    closing = cv2.morphologyEx(img.astype(np.uint8), cv2.MORPH_CLOSE, kernel)
+
+    return closing
+
+def get_binary_image(img, ksize, verbose=False):
+    # Apply each of the thresholding functions
+    gradx = abs_sobel_thresh(img, orient='x', sobel_kernel=ksize, thresh=(20, 100))
+    grady = abs_sobel_thresh(img, orient='y', sobel_kernel=ksize, thresh=(20, 100))
+    mag_binary = mag_thresh(img, sobel_kernel=ksize, mag_thresh=(30, 100))
+    dir_binary = dir_threshold(img, sobel_kernel=ksize, thresh=(0.7, 1.3))
+    S_binary = color_threshold(img, thresh=(170, 255))
+    # Combine thresholds
+    combined = np.zeros_like(dir_binary)
+    combined = np.logical_or(gradx, S_binary)
+
+    # Apply morphology
+    combined = morphology(combined, ksize=(3,3))
+
+    if verbose == True:
+        plt.imshow(combined, cmap='gray')
+        plt.title('S_binary_gradx_morpho_combined')
+        plt.show()
+
+    return combined
+#----------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+
+    img = mpimg.imread('/home/yuchao/CarND-Advanced-Lane-Lines/test_images/test6.jpg')
+    ksize = 3
+    binary_image = get_binary_image(img, ksize, verbose=True)
