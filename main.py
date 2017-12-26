6c13e4a3886f08162a3474bd49b3e1a8427187b9
York_Yin <yinyuchao1990@hotmail.com>
2017-12-26 09:15:32 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from camera_calibration import calibration, undistort
+from moviepy.editor import VideoFileClip
+from image_binarization import get_binary_image
+from perspective_transform import perspective
+from line_find import sliding_window_search, line_search_previous, project_on_to_original_image, Line
+import numpy as np
+import cv2
+
+counter = 0 # counter of processed frames
+line_left = Line(buffer_len=9)  # line on the left of the lane
+line_right = Line(buffer_len=9) # line on the right of the lane
+def pipline(frame):
+    global line_left, line_right, counter
+
+    undistorted = undistort( frame, mtx, dist, verbose=False)
+    binary_image = get_binary_image(undistorted, ksize=3, verbose=False)
+    warped, M, Minv = perspective(binary_image, verbose=False)
+    if counter > 0 and line_left.detected and line_right.detected:
+        line_right, line_left, out_img = line_search_previous(warped, line_left, line_right
+                                                               , verbose=False)
+    else:
+        line_left, line_right, out_img = sliding_window_search(warped, line_left, line_right
+                                                              , verbose=False)
+
+    draw = project_on_to_original_image(undistorted,  warped, Minv
+                                        , line_left, line_right, verbose=False)
+    # add curvature
+    R_left, R_right, R_mean = curvature(line_left, line_right, draw)
+    cv2.putText(draw, 'Radius of Curvature: {:.02f}(m)'.format(R_mean), (100, 60)
+                , cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2, cv2.LINE_AA)
+    # add position offset
+    position_offset = vehicle_position(line_left, line_right, draw)
+    cv2.putText(draw, 'Vehicle position offset from center: {:.02f}(m)'.format(position_offset), (100, 120)
+                , cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2, cv2.LINE_AA)
+    counter += 1
+    return draw
+
+def curvature(line_left, line_right, frame):
+    y_eval = frame.shape[0]/2
+    [A_left, B_left,_] = np.mean(line_left.recent_fits_meter, axis=0)
+    R_left = ((1 + (2 * A_left * y_eval + B_left) ** 2) ** 1.5) / np.absolute(2 * A_left)
+    [A_right, B_right,_] = np.mean(line_right.recent_fits_meter, axis=0)
+    R_right = ((1 + (2 * A_right * y_eval + B_right) ** 2) ** 1.5) / np.absolute(2 * A_right)
+    R_mean = np.mean([R_left, R_right])
+    return R_left, R_right, R_mean
+
+def vehicle_position(line_left, line_right, frame):
+    w = frame.shape[1]
+    # calculate the distance between two lines
+    if line_right.detected and line_left.detected:
+        left_bottom = np.mean(line_left.all_x[line_left.all_y > 0.95 * line_left.all_y.max()])
+        right_bottom = np.mean(line_right.all_x[line_right.all_y > 0.95 * line_right.all_y.max()])
+        width = right_bottom - left_bottom
+        position_offset = left_bottom + width/2 - frame.shape[1]/2
+        position_offset *= 3.7/700.
+
+    return position_offset
+
+
+
+if __name__ == '__main__':
+    cali_path = '/home/yuchao/CarND-Advanced-Lane-Lines/camera_cal'
+    pickle_file = '/home/yuchao/CarND-Advanced-Lane-Lines/pick'
+    img_cal = mpimg.imread('/home/yuchao/CarND-Advanced-Lane-Lines/camera_cal/calibration2.jpg')
+    ret, mtx, dist, rvecs, tvecs = calibration(img_cal, pickle_file, cali_path, verbose=False)
+    #img = mpimg.imread('/home/yiy6szh/CarND-Advanced-Lane-Lines/test_images/test2.jpg')
+    #dst = undistort(img, mtx, dist, verbose=True)
+
+    video = '/home/yuchao/CarND-Advanced-Lane-Lines/project_video.mp4'
+    clip = VideoFileClip(video)
+    clip1 = clip.fl_image(pipline)
+    out_video = '/home/yuchao/CarND-Advanced-Lane-Lines/out_video.mp4'
+    clip1.write_videofile(out_video, audio=False)
---
+++ perspective_transform.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from camera_calibration import calibration, undistort
+from image_binarization import get_binary_image
+
+
+def perspective(img, verbose=False):
+
+    h, w = img.shape[:2]
+
+    src = np.float32([[1065, 684],
+                      [255, 684],
+                      [603, 443],
+                      [678, 443]])
+    dst = np.float32([[w*4/5, h],
+                      [w/5, h],
+                      [w/5, 0],
+                      [w*4/5, 0]])
+
+    M = cv2.getPerspectiveTransform(src, dst)
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    warped = cv2.warpPerspective(img, M, (w, h), flags=cv2.INTER_LINEAR)
+
+    if verbose == True:
+        f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+        f.tight_layout()
+        ax1.imshow(img)
+        ax1.set_title('Original Image', fontsize=50)
+        ax2.imshow(warped)
+        ax2.set_title('Warped Result', fontsize=50)
+        plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+        plt.show()
+
+    return warped, M, Minv
+
+if __name__ == '__main__':
+    cali_path = '/home/yuchao/CarND-Advanced-Lane-Lines/camera_cal'
+    pickle_file = '/home/yuchao/CarND-Advanced-Lane-Lines/pick'
+    img = mpimg.imread('/home/yuchao/CarND-Advanced-Lane-Lines/test_images/straight_lines1.jpg')
+    #plt.imshow(img)
+    #plt.show()
+    ret, mtx, dist, rvecs, tvecs = calibration(img, pickle_file, cali_path, verbose=False)
+    dst = undistort(img, mtx, dist, verbose=False)
+    binary_image = get_binary_image(dst, 3, True)
+    warped, M = perspective(binary_image, verbose=True)
